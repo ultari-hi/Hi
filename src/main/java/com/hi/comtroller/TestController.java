1b23464987abd94d47b4c3982b736cf0e93efb7e
seoul-chan <kkchan9210@gmail.com>
2022-06-02 12:57:59 +0900

    test commit

---
+++ src/main/java/com/hi/comtroller/TestController.java
@@ -0,0 +1,26 @@
+package com.hi.comtroller;
+
+import com.hi.domain.TestDto;
+import com.hi.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+    @Autowired
+    TestService testService;
+//
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/test")
+    public List<TestDto> test() {
+
+        return testService.getAllDataList();
+    }
+}
