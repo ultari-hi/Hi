f79feb24271880bdc39ea1504b225e5f9289007f
park9707 <psw1436@naver.com>
2022-10-16 12:37:54 +0900

    JWT Token

--- src/main/java/com/hi/config/CorsConfig.java
+++ src/main/java/com/hi/config/CorsConfig.java
@@ -1,10 +1,10 @@
 package com.hi.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
@@ -13,11 +13,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); //응답할 때 json을 자카스크립트에서 처리할 수 있게 하는 것
+        config.setAllowCredentials(true); //응답할 때 json 을 자바스크립트에서 처리할 수 있게 하는 것
         config.addAllowedOrigin("*"); //모든 ip에 응답을 허용
-        config.addAllowedHeader("*"); //모든 header에 응답을 허용
+        config.addAllowedHeader("*"); //모든 헤더에 응답을 허용
         config.addAllowedMethod("*"); //모든 post, get, put, delete, patch 요청을 허용
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter();
+
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
